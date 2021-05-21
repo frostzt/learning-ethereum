03f12ce7fced2382178d292f49806ac2d82dbe47
frostzt <aidenfrostbite@gmail.com>
2021-05-21 10:02:44 +0530

    working on minter

--- dbank/src/contracts/Token.sol
+++ dbank/src/contracts/Token.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >0.5.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+  address public minter;
 
   //add minter changed event
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
+    minter = msg.sender;
   }
 
   //Add pass minter role function
 
   function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error, msg.sender does not have the minter role!');
 		_mint(account, amount);
 	}
 }
